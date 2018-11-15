8817fb9eccc441326b864133f5908d41f17e336d
rscheinberg <rscheinberg@stuy.edu>
2018-11-14 22:17:32 -0500

    added changes, not working 100% yet

--- pargs.c
+++ pargs.c
@@ -3,19 +3,24 @@
 #include <string.h>
 #include <unistd.h>
 
-char ** parse_args(char * line){
-  char ** arr = malloc(100);                //sizeof(*line)); idk why it doesnt work, replace later
-  printf("it good\n");
-  strcpy(oof,line);
-  for (int i = 0; oof; i++){
-    arr[i] = strsep(&oof," ");
-    printf("%s\n",arr[i]);
-  }
+char ** parse_args(char * line) {
+    
+    char **arr = malloc(6 * sizeof(char *));
+    
+    char *new_line = malloc(strlen(line));
+    strcpy(new_line, line);
+    
+    for (int i = 0; new_line; i++) {
+        arr[i] = strsep(&new_line," ");
+    }
+    
+    return arr;
 }
 
-
-int main(int argc, char * argv[]){
-  char ** args = parse_args(argv[1]);
-  execvp(args[0],args);
-  return 0;
+int main(int argc, char * argv[]) {
+    
+    char **args = parse_args(argv[1]);
+    execvp(args[0],args);
+    
+    return 0;
 }
